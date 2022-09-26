373df94ab2c75853bda07dc2118ca10dd9bd7f2c
JelenaTomic <tomic.elena09@gmail.com>
2022-09-25 22:51:06 -0500

    made routes

---
+++ controllers/api/category-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Category } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//get all categories
+router.get('/', withAuth, (req, res) => {
+    Category.findAll({
+        attributes: [
+        'id',
+        'category_name'
+    ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,115 @@
+const router = require('express').Router();
+const { Category, Workout, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+//get all categories for dashboard
+
+router.get('/', withAuth, (req, res) => {
+    Category.findAll({
+        attributes: [
+        'id',
+        'category_name'
+    ]
+    })
+    .then(dbCategoryData => {
+        // serialize data before passing to template
+        const categories = dbCategoryData.map(category => category.get({ plain: true }));
+        res.render('dashboard', { categories, loggedIn: true });
+    })
+    // error message
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+//get routes for each body group
+router.get('/:category_name', withAuth, (req, res) => {
+    Category.findOne({
+        where: {
+            category_name: req.params.category_name
+        }, 
+        attributes: [
+            'id',
+            'category_name', 
+            'created_at'
+        ],
+            include: [
+                {
+                    model: Workout,
+                    attributes: ['id','workout_name', 'workout_image', 'workout_url', 'created_at']
+                }
+            ]
+    })
+    .then(dbCategoryData => {
+        if (dbCategoryData) {
+            // serialize data before passing to template
+            const category = dbCategoryData.get({ plain: true });
+
+            res.render('category', {
+                category,
+                loggedIn: true
+            });
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+
+//get route: individual workouts
+router.get('/workout/:workout_name', withAuth, (req, res) => {
+    Workout.findOne({
+        where: {
+            workout_name: req.params.workout_name
+        }, 
+        attributes: [
+            'id',
+            'workout_name',
+            'workout_image',
+            'workout_url',
+            'created_at'
+        ],
+            include: [
+                {
+                    model: Category,
+                    attributes: ['id','category_name', 'created_at']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'user_id', 'workout_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+    })
+    .then(dbWorkoutData => {
+        if (dbWorkoutData) {
+            // serialize data before passing to template
+            const workout = dbWorkoutData.get({ plain: true });
+
+            res.render('single-post', {
+                workout,
+                loggedIn: true
+            });
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+// exports routers
+module.exports = router;
+
+
+
