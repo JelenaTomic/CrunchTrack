5dabfd2dd7c846e4881fec03934c8a734bc4acff
Nchirico04 <chirico.nick33@gmail.com>
2022-09-25 22:32:24 -0500

    made modules

---
+++ models/Workout.js
@@ -0,0 +1,77 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Workout extends Model {}
+
+// sets up the workout model to associate with index.js
+// the datatypes will define whether by numeric or
+// string
+
+Workout.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        workout_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        workout_image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        workout_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        weight: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate:{
+                isNumeric:true
+            }
+        },
+        reps: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate:{
+                isNumeric:true
+            }
+        },
+        sets:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            validate:{
+                isNumeric:true
+            }
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id'
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'workout'
+    }
+);
+
+module.exports = Workout;
